4f19501e2260bec4b961acf1cbad8e69ed55edf4
Chris Ward <chris.ward@improving.com>
2026-06-24 19:36:32 -0500

    updates to first page and nav

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,12 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Merge tailwind classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// This check can be removed, it is just for tutorial purposes
+// Make sure we have env vars for supabase set
 export const hasEnvVars =
   process.env.NEXT_PUBLIC_SUPABASE_URL &&
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
